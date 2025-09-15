5a15593ce39d4431ae3fe82566ab9d8604f3dd93
Ian Ra <ira@frba.utn.edu.ar>
2025-09-15 17:42:40 -0300

    Agregue verificaciones

--- services/pedidoService.js
+++ services/pedidoService.js
@@ -1,3 +1,4 @@
+import { EstadoPedido } from "../models/entities/estadoPedido.js";
 import {Pedido} from "../models/entities/pedido.js"
 import { PedidoRepository } from "../models/repositories/pedidoRepository.js";
 import UsuarioRepository from "../models/repositories/usuarioRepository.js";
@@ -36,6 +37,7 @@ export default class PedidoService {
             nuevoPedido = null
             return Promise.reject({name: "StockError", message: "No hay stock suficiente para completar el pedido"});
         }
+
         const pedidoGuardado = this.pedidoRepository.crearPedido(nuevoPedido);
         return Promise.resolve({
             data: pedidoGuardado,
@@ -55,13 +57,43 @@ export default class PedidoService {
         if(!usuario) {
             return Promise.reject({name: "NotFoundError", message: "Usuario no encontrado"});
         }
-        // Falta chequear que el vendedor venda los productos del pedido
-        
 
-        //SWITCH DE ESTADOS
-        if(pedidoData.estado) {
-            pedido.actualizarEstado(pedidoData.estado, usuario, pedidoData.motivo);
-        }
+        if(pedidoData.estado && pedidoData.estado !== pedido.estado) {
+            switch(pedidoData.estado) {
+                case EstadoPedido.Confirmado:
+                    if(pedido.estado !== EstadoPedido.Pendiente) {
+                        return Promise.reject({name: "StateError", message: "El pedido no puede ser confirmado"});
+                    }
+                    break;
+                case EstadoPedido.EnPreparacion:
+                    if(pedido.estado !== EstadoPedido.Confirmado) {
+                        return Promise.reject({name: "StateError", message: "El pedido no puede ser preparado"});
+                    }
+                    break;
+                case EstadoPedido.Enviado:
+                    if(pedido.estado !== EstadoPedido.EnPreparacion) { 
+                        return Promise.reject({name: "StateError", message: "El pedido no puede ser enviado"});
+                    }
+                    // if(!pedido.items.every((item) => item.producto.vendedor === usuario)) {
+                    //     return Promise.reject({name: "SellerError", message: "El usuario no vende los productos de este pedido"});
+                    // }
+                    // pedido.items.every((item) => item.producto.reducirStock(item.cantidad));
+                    break;
+                case EstadoPedido.Entregado:
+                    if(pedido.estado !== EstadoPedido.Enviado) { 
+                        return Promise.reject({name: "StateError", message: "El pedido no puede ser entregado"});
+                    }
+                    break;
+                case EstadoPedido.Cancelado:
+                    if(pedido.estado === EstadoPedido.Enviado || pedido.estado === EstadoPedido.Entregado) { 
+                        return Promise.reject({name: "StateError", message: "El pedido no puede ser cancelado"});
+                    }
+                    break;
+                default:
+                    return Promise.reject({name: "StateError", message: "Estado no válido"});
+            }
+        pedido.actualizarEstado(pedidoData.estado, usuario, pedidoData.motivo);
+    }
 
         return Promise.all([this.pedidoRepository.actualizarPedido(pedidoId, pedido)])
          .then((pedidoRes) => {
